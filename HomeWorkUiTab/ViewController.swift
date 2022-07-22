5f5676abe985592d136ba2d127051d5466211547
Ivan Osipov <ivan.loft72@gmail.com>
2022-07-23 00:10:17 +0500

    Initial Commit

---
+++ HomeWorkUiTab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkUiTab
+//
+//  Created by Иван Осипов on 23/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
