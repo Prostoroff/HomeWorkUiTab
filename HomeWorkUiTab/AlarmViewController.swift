084211e42907367f7d979c33ac6d29b92e0b8564
Ivan Osipov <ivan.loft72@gmail.com>
2022-08-15 23:00:21 +0500

    - создан alertViewController - в Alert.storyboard добавлена таблица для отображения заданных будильников - создана кастомная ячейка таблицы AlertTableViewCell

---
+++ HomeWorkUiTab/AlarmViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AlarmViewController.swift
+//  HomeWorkUiTab
+//
+//  Created by Иван Осипов on 15/8/22.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+    
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        settingButton.layer.cornerRadius = settingButton.bounds.height * 0.5
+        settingButton.clipsToBounds = true
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UINib(nibName: "AlarmTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+    }
+}
+
+extension AlarmViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! AlarmTableViewCell
+        
+        return cell
+    }
+}
+
+extension AlarmViewController: UITableViewDelegate {
+    
+}
